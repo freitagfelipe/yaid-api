2b9f024a01bcf0c5bc260cc73bf3fe9a704014c4
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2022-07-23 00:27:49 -0300

    Update auth middleware code

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -2,12 +2,14 @@ import { NextFunction, Request, Response } from "express";
 import { APIError } from "../types/types";
 
 export function auth(req: Request, _res: Response, next: NextFunction): void {
-    if (req.get("TOKEN") === undefined) {
+    const { authorization: token } = req.headers;
+
+    if (token === undefined) {
         return next({
             statusCode: 400,
             message: "No token provided",
         } as APIError);
-    } else if (req.get("TOKEN") != process.env.TOKEN) {
+    } else if (token != process.env.TOKEN) {
         return next({
             statusCode: 401,
             message: "Invalid token",

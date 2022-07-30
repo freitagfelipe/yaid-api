76a8a54e9ab072d02c4284e48210c77127d7d2a0
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2022-07-30 03:54:00 -0300

    Update validator middleware to work with fetch post and stories routers

--- src/middlewares/validator.middleware.ts
+++ src/middlewares/validator.middleware.ts
@@ -6,14 +6,19 @@ export function validator(
     _res: Response,
     next: NextFunction
 ): void {
-    const { url } = req.query;
+    const isFetchPost = req.baseUrl.includes("fetch-post");
+    const { url, user } = req.query;
 
-    if (url === undefined) {
+    if (
+        (isFetchPost && url === undefined) ||
+        (!isFetchPost && user === undefined)
+    ) {
         return next({
             statusCode: 400,
-            message: "No url provided",
+            message: `No ${isFetchPost ? "url" : "user"} provided`,
         } as APIError);
     } else if (
+        url &&
         !/(https:\/\/)?www.instagram.com\/(p|reel|tv)/.test(url as string)
     ) {
         return next({

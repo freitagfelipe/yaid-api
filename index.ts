5212a5a18a38233b4014abda883c45aab36cb69b
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2023-04-21 00:17:09 -0300

    Add cors and add get content route

--- index.ts
+++ index.ts
@@ -1,12 +1,28 @@
-import "dotenv/config";
 import express, { NextFunction, Request, Response } from "express";
-import { auth } from "./src/middlewares/auth.middleware";
+import cors from "cors";
+import "dotenv/config";
 import { router as fetchPost } from "./src/routes/fetchPost.route";
 import { router as fetchStories } from "./src/routes/fetchStories.route";
-import { APIError } from "./src/types/types";
+import { router as getContent } from "./src/routes/getContent.route";
+import { APIError, IAPIError } from "./src/types/types";
 
 const app = express();
-const port: number = (process.env.PORT as unknown as number) || 3000;
+const port: number = process.env.PORT as unknown as number;
+
+app.use(
+    cors({
+        origin(requestOrigin, callback) {
+            if (
+                requestOrigin === process.env.CORS_BOT! ||
+                requestOrigin === process.env.CORS_WEB!
+            ) {
+                callback(null, true);
+            } else {
+                callback(new APIError("CORS not allowed", 401));
+            }
+        },
+    })
+);
 
 app.get("/", (_req: Request, res: Response) => {
     res.json({
@@ -14,13 +30,13 @@ app.get("/", (_req: Request, res: Response) => {
     });
 });
 
-app.use(auth);
-
 app.use("/fetch-post", fetchPost);
 
 app.use("/fetch-stories", fetchStories);
 
-app.use((err: APIError, _req: Request, res: Response, _next: NextFunction) => {
+app.use("/get-content", getContent);
+
+app.use((err: IAPIError, _req: Request, res: Response, _next: NextFunction) => {
     const { statusCode, message } = err;
 
     res.status(statusCode).json({ message });
